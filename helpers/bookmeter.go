feb677259f00bfb82b26fab06e057e80cfea3aa5
Xythh <121327029+xythh@users.noreply.github.com>
2023-06-12 14:47:20 -0500

    Fixed gitignore and directory changes.

---
+++ helpers/bookmeter.go
@@ -0,0 +1,6 @@
+package main
+
+// bookmeter stuff goes in here, parsing it and turned that output into something useable.
+
+func parseHTML() {
+}
--- helpers/vndb.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func getVnByName() {
-
-}
-
-func getUserList()
